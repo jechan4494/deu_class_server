326005fcaa3ac6de6ee0fe21ed6f78f380806b53
KimGwangHoon12 <wwnr2qqkb2@naver.com>
2025-05-25 00:30:11 +0900

    [학사 조교 코드 붙여넣기]

---
+++ src/main/java/controller/ta/ManagerController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.ta;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import server.model.ta.ReservationModel;
+
+import java.util.List;
+
+public class ManagerController {
+    private ReservationModel model = new ReservationModel();
+
+    public JSONArray getAllUsersAsJson() {
+        List<String[]> users = model.loadUsers();
+        JSONArray arr = new JSONArray();
+
+        for (String[] row : users) {
+            JSONObject obj = new JSONObject();
+            obj.put("id", row[0]);
+            obj.put("name", row[1]);
+            obj.put("role", row[2]);
+            obj.put("department", row[3]);
+            arr.put(obj);
+        }
+        return arr;
+    }
+
+    public boolean deleteUser(String userId) {
+        return model.deleteUser(userId);
+    }
+}
